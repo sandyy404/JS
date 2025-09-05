7e5a206faa366078719bc9adb7a061fccd9a4d02
sandyy404 <sp8251960@gmail.com>
2025-09-05 21:10:03 +0530

    next commit

---
+++ 05-09-2025/non-primitive.js
@@ -0,0 +1,39 @@
+// const username = {
+//   firstname:"Sandeep",
+//   "middle name" :"Kumar",
+//   email_id: "name@gmail.com",
+// //   phone_no: 0123456789
+
+// }
+// console.log(username.firstname);
+// console.log(username["middle name"]);
+
+// console.log(username.lastname);
+// username.firstname = "happy"
+// username.lastname = "Pandey"
+// console.log(username.firstname);
+// console.log(username.lastname);
+
+// console.log(username);
+
+// console.log(typeof username);
+
+
+
+
+// let today = new Date()
+// console.log(today.getDate());
+
+
+
+//  Array
+let array = ["Sandeep", true];
+// console.log(array);
+
+let isValue = "2abc"
+console.log(Number(isValue));  // it wil show NaN means not a number but the type will show number
+console.log(typeof Number(isValue));
+console.log(Number(undefined));
+
+
+
