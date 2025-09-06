d8914ba4f47f869e285f99ea55aaf535eb5350ab
sandyy404 <sp8251960@gmail.com>
2025-09-07 00:33:40 +0530

    updated

--- 06-09-2025/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-esVisited = ["Mumbai", "Sydney"]
-// // citiesVisited[citiesVisited.length] = "Berlin"
-// citiesVisited.push("Berlin")  // this is push method.
-// citiesVisited.forEach(function(cities,index){
-//    console.log(index+1 +": "+ cities);
-   
-// })
-
