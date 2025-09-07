740a40fb3f833069047eae073a2bd2af086d850a
sandyy404 <sp8251960@gmail.com>
2025-09-08 00:16:13 +0530

    updated

--- 06-09-2025/ChallengeinArrayandMethod.js
+++ 06-09-2025/ChallengeinArrayandMethod.js
@@ -1,73 +1,75 @@
-// Problem1
-/* Declare an array named teaFlavors that contains the strings "green tea", "black tea", and "oolong tea".Access the first element of the array and store it in a variable named firstTea. */
+// // Problem1
+// /* Declare an array named teaFlavors that contains the strings "green tea", "black tea", and "oolong tea".Access the first element of the array and store it in a variable named firstTea. */
 
-let teaFlavors = ["Green tea", "Black tea", "Oolong tea"]
+// let teaFlavors = ["Green tea", "Black tea", "Oolong tea"]
 
-// let teaFl = new Array("Green tea", "Black tea", "Oolong tea")  another way to declare array
-
-
-/* teaFlavors.forEach(function (flavor, index) {  this is for accessing the whole element of the array
-   console.log(index+1 + ":" + flavor);
-}
-) */
-console.log( teaFlavors[0]);
-let firstTea = teaFlavors[0];
+// // let teaFl = new Array("Green tea", "Black tea", "Oolong tea")  another way to declare array
 
 
-
-// Problem2
-
-/* Declare an array named cities containing "London", "Tokyo", "Paris", and "New York".Access the third element in the array and store it in a variable named favoriteCity. */
-
-
-let cities = ["London", "Tokyo", "Paris", "New York"]
-
-console.log(cities[2]);
-const favoriteCity = cities[2];
+// /* teaFlavors.forEach(function (flavor, index) {  this is for accessing the whole element of the array
+//    console.log(index+1 + ":" + flavor);
+// }
+// ) */
+// console.log( teaFlavors[0]);
+// let firstTea = teaFlavors[0];
 
 
 
-// Problem3
+// // Problem2
 
-/* You have an array named teaTypes containing "herbal tea", "white tea", and "masala chai". Change the second element of the array to "jasmine tea". */
+// /* Declare an array named cities containing "London", "Tokyo", "Paris", and "New York".Access the third element in the array and store it in a variable named favoriteCity. */
 
-let teaTypes =["herbal tea", "white tea", "masala chai"]
-teaTypes[1]= "jasmine tea"
-teaTypes.forEach(function(tea,index){
-   console.log(index+1 +": "+ tea);
+
+// let cities = ["London", "Tokyo", "Paris", "New York"]
+
+// console.log(cities[2]);
+// const favoriteCity = cities[2];
+
+
+
+// // Problem3
+
+// /* You have an array named teaTypes containing "herbal tea", "white tea", and "masala chai". Change the second element of the array to "jasmine tea". */
+
+// let teaTypes =["herbal tea", "white tea", "masala chai"]
+// teaTypes[1]= "jasmine tea"
+// teaTypes.forEach(function(tea,index){
+//    console.log(index+1 +": "+ tea);
    
-})
+// })
  
-// Problem4
-/* Declare an array named citiesVisited containing "Mumbai" and "Sydney". Add "Berlin" to the array using the push method. */
+// // Problem4
+// /* Declare an array named citiesVisited containing "Mumbai" and "Sydney". Add "Berlin" to the array using the push method. */
 
-let citiesVisited = ["Mumbai", "Sydney"]
-// citiesVisited[citiesVisited.length] = "Berlin"
-citiesVisited.push("Berlin")  // this is push method.
-citiesVisited.forEach(function(cities,index){
-   console.log(index+1 +": "+ cities);
+// let citiesVisited = ["Mumbai", "Sydney"]
+// // citiesVisited[citiesVisited.length] = "Berlin"
+// citiesVisited.push("Berlin")  // this is push method.
+// citiesVisited.forEach(function(cities,index){
+//    console.log(index+1 +": "+ cities);
    
-})
+// })
 
 
  
-// Problem5
-/* 
-You have an array named teaOrders with "chai", "iced tea", "matcha", and "earl grey".Remove the last element of the array using the pop method and store it in a variable named lastOrder. */
+// // Problem5
+// /* 
+// You have an array named teaOrders with "chai", "iced tea", "matcha", and "earl grey".Remove the last element of the array using the pop method and store it in a variable named lastOrder. */
 
 
-let teaOrders = ["chai", "iced tea", "matcha", "earl grey"]
-const lastOrder = teaOrders.pop();  // this pop method will automatically remove the last element
-console.log(teaOrders);
-console.log(lastOrder);
+// let teaOrders = ["chai", "iced tea", "matcha", "earl grey"]
+// const lastOrder = teaOrders.pop();  // this pop method will automatically remove the last element
+// console.log(teaOrders);
+// console.log(lastOrder);
 
 // Problem6
 
-/* You have an array named popularTeas containing "green tea", "oolong tea", and "chai".Create a soft copy of this array named softCopyTeas. */
+/* You have an array named popularTeas containing "green tea", "oolong tea", and "chai".Create a softcopy of this array named softCopyTeas. */
 
-let popularTeas = ["green tea", "oolong tea","chai"]
-const softCopyTeas = popularTeas
-console.log(softCopyTeas);
+// let popularTeas = ["green tea", "oolong tea","chai"]
+// const softCopyTeas = popularTeas
+// // popularTeas.pop();
+// console.log(popularTeas);
+// console.log(softCopyTeas);
 
 
 
@@ -76,7 +78,44 @@ console.log(softCopyTeas);
 
 /* You have an array named topCities containing "Berlin", "Singapore", and "New York".Create a hard copy of this array named hardCopyCities. */
 
-let topCities = ["Berlin", "Singapore", "New York"]
+// let topCities = ["Berlin", "Singapore", "New York"]
+// // const  hardCopyCities = [...topCities];
+// let hardCopyCities = topCities.slice();
+// topCities.pop();
+// console.log(topCities);
+// console.log(hardCopyCities);
 
 
+// Problem8
+/* You have two arrays: europeanCities containing "Paris" and "Rome", and "asianCities" containing "Tokyo" and "Bangkok". Merge these two arrays into a new array named worldCities. */
+// let europeanCities  = ["Paris" , "Rome"]
+// let asianCities = ["Tokyo", "Bangkok"]
+// // let worldCities = [europeanCities +","+ asianCities];
+// let worldCities = europeanCities.concat(asianCities);
+// console.log(worldCities);
+// console.log(typeof worldCities);
+
+
+
+
+
+// Problem9
+/*  You have an array named teaMenu containing "masala chai", "oolong tea", "green tea", and "earl grey". Find the length of the array and store it in a variable name menulength. */
+
+// let teaMenu = ["masala chai", "oolong tea", "green tea", "earl grey"]
+// let menulength= teaMenu.length
+// console.log(menulength);
+
+
+
+// Problem10
+ /* You have an array named `cityBucketList` containing "Kyoto", "London", "Cape Town", and "Vancouver". Check if "London" is in the array and store the result in a variable named `isLondonInList`. */
+
+
+ let cityBucketList = ["Kyoto", "London", "Cape Town",  "Vancouver"]
+ let isLondonInList = cityBucketList.includes("London")
+ console.log(isLondonInList);
+ console.log(typeof isLondonInList);
+ 
+ 
 
