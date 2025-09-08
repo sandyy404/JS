47d3b2e67e2a752517eeeb67950cdbae69da93a4
sandyy404 <sp8251960@gmail.com>
2025-09-08 23:58:28 +0530

    updated

--- 08-09-2025/loop1.js
+++ 08-09-2025/loop1.js
@@ -2,13 +2,13 @@
 
 // Write a for loop that loops through the array ["green tea", "black tea", "chai", "oolong tea"] and stops the loop when it finds "chai". Store all teas before "chai" in a new array named selectedTeas.
 
-// let array = ["green tea", "black tea", "chai", "oolong tea"]
-// let selectedTeas= []
-// for(let index =0; array[index] != "chai";index++)
-// {
-//    selectedTeas.push(array[index])
-// }
-// console.log(selectedTeas);
+let array = ["green tea", "black tea", "chai", "oolong tea"]
+let selectedTeas= []
+for(let index =0; array[index] != "chai";index++)
+{
+   selectedTeas.push(array[index])
+}
+console.log(selectedTeas);
 
 
 
@@ -18,14 +18,14 @@
 
 // Write a for loop that loops through the array["London", "New York", "Paris", "Berlin"] and skips "Paris". Store the other cities in a new array named visitedCities.
 
-// let array = ["London", "New York", "Paris", "Berlin"]
-// let visitedCities = []
-// for (let index = 0; index < array.length; index++) {
-//    if (array[index] == "Paris") {
-//       continue;
-//    } else {
-//       visitedCities.push(array[index])
-//    }
-// }
+let array1 = ["London", "New York", "Paris", "Berlin"]
+let visitedCities = []
+for (let index = 0; index < array1.length; index++) {
+   if (array1[index] == "Paris") {
+      continue;
+   } else {
+      visitedCities.push(array[index])
+   }
+}
 
-// console.log(visitedCities);
+console.log(visitedCities);
