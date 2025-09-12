e14cbbce30f8b428bff240b9b23d68df30c9ac50
sandyy404 <sp8251960@gmail.com>
2025-09-12 21:52:24 +0530

    updated

---
+++ 12-09-2025/prototypes.js
@@ -0,0 +1,10 @@
+let computer = { cpu: 18 }
+let Hp = { 
+   screen: "fullHd",
+   __proto__ :computer,
+ };
+let Hardware = {}
+// console.log(`computer`, computer.__proto__);
+
+
+console.log(`Hp`,Hp.__proto__);
