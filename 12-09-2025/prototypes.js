fbd633e1cd82e19789e885b765548ab1e9524ef8
sandyy404 <sp8251960@gmail.com>
2025-09-16 00:49:40 +0530

    updated

--- 12-09-2025/prototypes.js
+++ 12-09-2025/prototypes.js
@@ -1,3 +1,5 @@
+// one way to get the access of the  properties from the object
+
 let computer = { cpu: 18 }
 let Hp = { 
    screen: "fullHd",
@@ -7,4 +9,16 @@ let Hardware = {}
 // console.log(`computer`, computer.__proto__);
 
 
-console.log(`Hp`,Hp.__proto__);
+console.log(`Hp`,Hp.__proto__);    //output will show { cpu: 18 } because it is overriden in Hp object
+ 
+
+// another way to get the access of the properties from the object.
+
+let generics = {tyres:10}
+let RR = {
+  owner:"Sandeep"
+}
+Object.setPrototypeOf(RR,generics);
+console.log(`RR`, Object.getPrototypeOf(RR));
+
+
