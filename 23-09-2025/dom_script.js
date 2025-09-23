76cf7c743d451637e78c20718889ef8f1a52a874
sandyy404 <sp8251960@gmail.com>
2025-09-23 21:14:31 +0530

    updated

--- 23-09-2025/dom_script.js
+++ 23-09-2025/dom_script.js
@@ -1,1 +1,26 @@
-alert("loading");
+// alert("loading");
+// "grap the element":-grabbing an element means selecting or accessing an element from the HTML document so you can work with it (usually using JavaScript).
+// So in short, "grabbing an element" means selecting it from the DOM (Document Object Model) so you can manipulate it (change text, color, style, events, etc.).
+
+
+// example:-1
+
+document.getElementById("changetextbutton").addEventListener("click", function () {
+   let paragraph = document.getElementById("myparagraph");
+   // console.log(paragraph);
+
+   paragraph.textContent = "The paragraph is changed!"  // this will change the paragraph text
+})
+
+
+// Traversing the DOM means moving around the DOM tree (up, down, or sideways) to access or modify elements.
+
+// example:-2
+
+document.getElementById("highlightthefirstcity").addEventListener("click", function () {
+   let citieslist = document.getElementById("citieslist");
+   // console.log(hold);
+   //   console.log(citylist.firstElementChild); this will show the first element of the citeslist.
+   citieslist.firstElementChild.classList.add("highlight")
+});
+
