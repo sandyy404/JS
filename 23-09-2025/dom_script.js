c451995ede26116c51a73cddbb560b55eee81e10
sandyy404 <sp8251960@gmail.com>
2025-09-25 01:23:17 +0530

    updated

--- 23-09-2025/dom_script.js
+++ 23-09-2025/dom_script.js
@@ -27,29 +27,59 @@ document.getElementById("highlightthefirstcity").addEventListener("click", funct
 
 //example:-3
 
-document.getElementById("changeOrder").addEventListener("click",function(){
+document.getElementById("changeOrder").addEventListener("click", function () {
    let coffee = document.getElementById("coffeetype");
    console.log(coffee);
    // coffee.
    coffee.textContent = "Lette";
    coffee.style.backgroundColor = "rgb(48, 92, 143)";
    coffee.style.padding = "3px;"
-   })
+})
 
 
 // example:-4
 
-document.getElementById("addNewItem").addEventListener("click",function(){
+document.getElementById("addNewItem").addEventListener("click", function () {
 
-   let newItem = document.createElement('li')  
+   let newItem = document.createElement('li')
    newItem.textContent = "Namkeen";           // these two lines will create the element.
    let order = document.getElementById("shoppinglist").appendChild(newItem)
 })
 
 
 // example:-5
-document.getElementById("removeLasttask").addEventListener("click",function(){
+document.getElementById("removeLasttask").addEventListener("click", function () {
    let remove = document.getElementById("taskList");
    remove.lastElementChild.remove()  // this will remove the last task
-})   
+})
 
+/* Event Bubbling and Capturing
+
+Bubbling (default): event moves from the target element → up through parents. 
+Capturing: event moves from root → down to target.
+*/
+
+// example:-6
+document.getElementById("clickMeButton").addEventListener("click", function () {
+   alert("Button clicked!");
+
+})
+
+
+//  example:-7
+/* Event delegation is a technique where instead of attaching event listeners to many child elements,you attach one event listener to a parent element and let the event bubble up to handle the action. */
+
+document.getElementById("tealist").addEventListener("click", function (event) {
+   if (event.target && event.target.matches(".teaItem")) {
+      alert("You selected : " + event.target.textContent);
+   }
+})
+
+/* Form handling means capturing user input from an HTML form and processing it using JavaScript (or later sending to a server).Typical steps:User enters data into form fields.JavaScript validates the input (before sending).Form submission is handled by JS or sent to a server. */
+
+
+// example:-8
+
+// document.getElementById("feedBackForm").addEventListener("submit",function(event){
+//  alert(event.)
+// })
