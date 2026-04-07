e675e909b718ecd31e6a5e1eacd2cbf5a01189a6
sandyy404 <sp8251960@gmail.com>
2026-04-07 13:45:55 +0530

    one more day

---
+++ Start/ThirdDay/JS_47.js
@@ -0,0 +1,35 @@
+const user = {
+   FirstName : "Sandeep",
+   SecondName : "Kumar",
+   'Third Name' : "Pandey",
+   
+}
+/* console.log(user);
+console.log(user.FirstName = "Happy");
+console.log(user['Third Name']); */
+
+// user = {                   Assignment to constant variable.
+//    FirstName : "Happy",
+//    SecondName : "Kumar",
+//    ThirdName : "Pandey",
+// }
+
+
+let today = new Date()
+// console.log(today.getDay());
+
+
+
+// Array
+let name = ["Sandeep","Shayam","Pradeep"]
+console.log(typeof name);
+
+//typecasting
+ let isval = true
+/*  console.log(true + 1);
+console.log("1" +2);
+console.log(2 + "1");
+console.log(typeof undefined); */
+
+ 
+
---
+++ Start/ThirdDay/JS_49.js
@@ -0,0 +1,46 @@
+/* 1:-  Declare an array named `teaFlavors that contains the strings `"green tea"`, `"black tea"`, and `"oolong tea".
+Access the first element of the array and store it in a variable named `firstTea`. */
+
+let teaFlavors =["green tea", "black tea", "oolong tea"] 
+firstTea = teaFlavors[0]
+console.log(firstTea);
+
+/*
+2. Declare an array named `cities` containing `"London"`, `"Tokyo"`, `"Paris"`, and `"New York"`. Access the third element in the array and store it in a variable named `favoriteCity`.
+*/
+
+let cities =["London","Tokyo","Paris","New York"]
+favoriteCity = cities[2]
+console.log(favoriteCity);
+
+/*
+3. You have an array named `teaTypes` containing `"herbal tea"`, `"white tea"`, and `"masala chai"`. Change the second element of the array to `"jasmine tea"`.
+*/
+ teaType =["herbal tea", "white tea", "masala chai"]
+ teaType[1] = "jasmine tea"
+ console.log(teaType);
+  
+ /*
+4. Declare an array named `citiesVisited` containing `"Mumbai"` and `"Sydney"`.
+Add `"Berlin"` to the array using the `push`method.
+*/
+citiesVisited = ["Mumbai","Sydney","Berlin"]
+citiesVisited.push("Delhi")
+citiesVisited.push("Jaipur")
+console.log(citiesVisited);
+
+/*
+5. You have an array named `teaOrders` with `"chai"`, `"iced tea"`, `"matcha"`, and `"earl grey"`.
+Remove the last element of the array using the `pop` method and store it in a variable named `lastOrder`.
+*/
+teaOrders = ["chai", "iced tea", "matcha", "earl grey"]
+lastorder = teaOrders.pop()
+console.log(teaOrders);
+console.log(lastorder);
+
+/*
+6. You have an array named `popularTeas` containing `"green tea"`, `"oolong tea"`, and `"chai"`.
+Create a soft copy of this array named `softCopy Teas`.
+*/
+
+
