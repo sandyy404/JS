92de0774944a118bdb6898b2f362ddcf02c29afe
sandyy404 <sp8251960@gmail.com>
2026-04-28 18:53:17 +0530

    fifth

---
+++ Start/FifthDay/oops.js
@@ -0,0 +1,10 @@
+let computer = {cpu :12,Ram : 12};
+let Hp = {
+   screen: "FHD",
+   __proto__: computer, // accessing the computer's details 
+};
+let hardware ={};
+
+// console.log(`Hp:-`,Hp.__proto__);
+console.log(hardware);
+
--- Start/FourthDay/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-function makeTea(typeofTea){
-//    return `Maketea : ${typeofTea}`
-// }
-// function processTea0rder(teafunction){
-//     return makeTea("earl grey");
-// }
-
-// let order = processTea0rder(makeTea)
-// console.log(order);
