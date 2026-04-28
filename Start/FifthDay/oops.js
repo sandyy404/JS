0052b3688e5e871c818ee7d1d45db53268c70bb7
sandyy404 <sp8251960@gmail.com>
2026-04-28 19:07:47 +0530

    h

--- Start/FifthDay/oops.js
+++ Start/FifthDay/oops.js
@@ -1,3 +1,4 @@
+// first method to accessing the one object from another by using __proto__. 
 let computer = {cpu :12,Ram : 12};
 let Hp = {
    screen: "FHD",
@@ -6,5 +7,19 @@ let Hp = {
 let hardware ={};
 
 // console.log(`Hp:-`,Hp.__proto__);
-console.log(hardware);
+// console.log(hardware);
+
+// second method
+
+let car = {
+   tyres: 5
+};
+let RR = {
+   price1 :"Phantom : 8.9cr",
+   price2 :"Ghost Series II : 10.52cr",
+   price3 : "Spectre: 7.5cr" 
+};
+Object.setPrototypeOf(RR,car)
+console.log(`RR`,Object.getPrototypeOf(RR));
+console.log(RR);
 
