c406484893a1c6c21aea5d08f40304f33958b1d7
sandyy404 <sp8251960@gmail.com>
2026-04-29 22:59:26 +0530

    oops

---
+++ Start/SixthDay/oops.js
@@ -0,0 +1,29 @@
+//Polymorphism
+class Bird{
+   fly(){
+      return `Flying....`
+   }
+}
+class Panguin extends Bird{
+   fly(){
+      return `Panguin can't fly`
+   }
+}
+let bird  = new Bird()
+let panguin = new Panguin()
+// console.log(panguin.fly());
+// console.log(bird.fly());
+
+// static method
+
+class Calculator{
+   static add(a,b){
+      return a+b
+   }
+}
+
+// let minical = new Calculator()
+// console.log(minical.add());     this will give error because static method can be called only by their call.
+
+console.log(Calculator.add(1,5));
+
---
+++ Start/SixthDay/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+fly(){
+   //    return `Panguin can't fly`
+   // }
